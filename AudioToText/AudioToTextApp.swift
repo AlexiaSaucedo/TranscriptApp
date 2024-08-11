7c207e55d191390582e70ea7aff0a187a587c239
AlexiaSaucedo <AlexiaSaucedo@users.noreply.github.com>
2024-08-11 14:00:59 -0600

    Initial Commit

---
+++ AudioToText/AudioToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioToTextApp.swift
+//  AudioToText
+//
+//  Created by Alexia Fernanda Saucedo Romero on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
